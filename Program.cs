9a961272b5e27b6d2d0b37faa61c29806701ab76
divetri <divetrirahmawati@gmail.com>
2022-03-18 07:54:22 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Quiz03.Solution.Soal1();
+Quiz03.Solution.Soal2();
+Quiz03.Solution.Soal3();
+Quiz03.Solution.Soal4();
+Quiz03.Solution.Soal5();
+Quiz03.Solution.Soal6();
+Quiz03.Solution.Soal7();
+Quiz03.Solution.Soal8();
+Console.ReadLine();
